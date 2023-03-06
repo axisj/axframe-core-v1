e5312eb6048c3a05b76daaedda1c112aca6a77e4
tom <tom@axisj.com>
2023-03-06 15:01:13 +0900

    remove wrong import

--- components/common/index.tsx
+++ components/common/index.tsx
@@ -4,5 +4,4 @@ export * from "./Spinner";
 export * from "./Loading";
 export * from "./ColResizer";
 export * from "./RowResizer";
-export * from "../../../components/common/EmptyMsg";
 export * from "./ProgramTitle";
