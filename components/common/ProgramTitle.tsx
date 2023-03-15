cb1f9b2b2c16aa684bc0a273dddb1c36a6105371
tom <tom@axisj.com>
2023-03-15 14:47:38 +0900

    Refactoring ProgramTitle

--- components/common/ProgramTitle.tsx
+++ components/common/ProgramTitle.tsx
@@ -62,12 +62,31 @@ function ProgramTitle({ title, icon, disableIcon, children }: Props) {
     [APP_MENUS, linkByRoute]
   );
 
-  const menuItems = React.useMemo(() => {
-    return breadCrumbs[breadCrumbs.length - 2]?.children.map((b, bidx) => {
+  const bItems = React.useMemo(() => {
+    return breadCrumbs.map((item, idx) => {
+      if (idx < breadCrumbs.length - 2) {
+        return {
+          key: item.keyPath.join("."),
+          title: item.multiLanguage[currentLanguage],
+        };
+      }
+
+      const menuItems = breadCrumbs[breadCrumbs.length - 2]?.children.map((b, bidx) => {
+        return {
+          key: b.keyPath?.join(".") ?? bidx + "",
+          label: b.multiLanguage[currentLanguage],
+          program_type: b.progCd,
+        };
+      });
+
       return {
-        key: b.keyPath?.join(".") ?? bidx + "",
-        label: b.multiLanguage[currentLanguage],
-        program_type: b.progCd,
+        key: item.keyPath.join("."),
+        title: item.multiLanguage[currentLanguage],
+        menu: menuItems
+          ? {
+              items: menuItems,
+            }
+          : undefined,
       };
     });
   }, [breadCrumbs, currentLanguage]);
@@ -78,26 +97,26 @@ function ProgramTitle({ title, icon, disableIcon, children }: Props) {
         ? null
         : icon ?? <MenuIcon typeName={iconTy ?? "Default"} color={"#0281FE"} secondColor={"#0281FE"} fontSize={22} />}
       <TitleWrap>{title}</TitleWrap>
-      <Breadcrumb>
-        {breadCrumbs.map((breadCrumb, idx) => {
-          return (
-            <Breadcrumb.Item
-              key={breadCrumb.keyPath.join(".")}
-              menu={
-                breadCrumbs.length - 1 === idx
-                  ? ({
-                      onClick: handleClickMenu,
-                      items: menuItems,
-                      selectedKeys: [breadCrumb.keyPath.join(".")],
-                      className: "breadcrumb-menu",
-                    } as any)
-                  : undefined
-              }
-            >
-              {breadCrumb.multiLanguage[currentLanguage]}
-            </Breadcrumb.Item>
-          );
-        })}
+      <Breadcrumb items={bItems}>
+        {/*{breadCrumbs.map((breadCrumb, idx) => {*/}
+        {/*  return (*/}
+        {/*    <Breadcrumb.Item*/}
+        {/*      key={breadCrumb.keyPath.join(".")}*/}
+        {/*      menu={*/}
+        {/*        breadCrumbs.length - 1 === idx*/}
+        {/*          ? ({*/}
+        {/*              onClick: handleClickMenu,*/}
+        {/*              items: menuItems,*/}
+        {/*              selectedKeys: [breadCrumb.keyPath.join(".")],*/}
+        {/*              className: "breadcrumb-menu",*/}
+        {/*            } as any)*/}
+        {/*          : undefined*/}
+        {/*      }*/}
+        {/*    >*/}
+        {/*      {breadCrumb.multiLanguage[currentLanguage]}*/}
+        {/*    </Breadcrumb.Item>*/}
+        {/*  );*/}
+        {/*})}*/}
       </Breadcrumb>
       {children}
     </Container>
