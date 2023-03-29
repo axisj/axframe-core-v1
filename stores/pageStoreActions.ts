9774acdbca42bbe12e5d811d8af4c251609c9fc2
tom <tom@axisj.com>
2023-03-29 14:33:39 +0900

    Fix init action

--- stores/pageStoreActions.ts
+++ stores/pageStoreActions.ts
@@ -1,4 +1,6 @@
 import { getMetaDataByPath, setMetaDataByPath, usePageTabStore } from "./usePageTabStore";
+import { UserService } from "../../services";
+import { ROUTES_LIST } from "../../router";
 
 interface PageStoreConfig {
   unSubscribe?: () => void;
@@ -6,9 +8,24 @@ interface PageStoreConfig {
 }
 
 export const pageStoreActions = (set, get, config?: PageStoreConfig) => ({
-  init: () => {
+  init: async () => {
     set({ routePath: location.pathname });
     const metaData = getMetaDataByPath(location.pathname);
+
+    const currentRoute = ROUTES_LIST.find((route) => route.path === location.pathname);
+    if (currentRoute) {
+      const data = await UserService.getProgramFn({ progCd: currentRoute.program_type });
+
+      const programFn = data.ds.reduce((acc, cur) => {
+        return { ...acc, [cur.key]: cur.label };
+      }, {});
+
+      if (metaData) {
+        metaData.programFn = programFn;
+      } else if (config?.createState) {
+        config.createState.programFn = programFn;
+      }
+    }
 
     if (metaData) {
       get().syncMetadata(metaData);
@@ -16,12 +33,23 @@ export const pageStoreActions = (set, get, config?: PageStoreConfig) => ({
       set(config.createState);
     }
   },
-  reset: () => {
+  reset: async () => {
     const routePath = get().routePath;
     if (!routePath) return;
 
     setMetaDataByPath(routePath, {});
+
     if (config?.createState) {
+      const currentRoute = ROUTES_LIST.find((route) => route.path === location.pathname);
+      if (currentRoute) {
+        const data = await UserService.getProgramFn({ progCd: currentRoute.program_type });
+        const programFn = data.ds.reduce((acc, cur) => {
+          return { ...acc, [cur.key]: cur.label };
+        }, {});
+
+        config.createState.programFn = programFn;
+      }
+
       set(config.createState);
     } else {
       get().syncMetadata();
