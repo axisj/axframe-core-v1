2c859e563b48d0242863ea362de5aa64ab4da1a4
tom <tom@axisj.com>
2023-03-30 13:15:07 +0900

    add allowClear

--- components/search/SearchParamInput.tsx
+++ components/search/SearchParamInput.tsx
@@ -5,7 +5,7 @@ import { SearchParamComponent } from "./SearchParam";
 export const SearchParamInput: SearchParamComponent = ({ name, placeholder, options, label, width, disabled }) => {
   return (
     <Form.Item name={name} {...(label ? { label, style: { marginBottom: 0, marginRight: 10 } } : { noStyle: true })}>
-      <Input placeholder={placeholder?.toString()} style={{ minWidth: 100, width }} disabled={disabled} />
+      <Input placeholder={placeholder?.toString()} style={{ minWidth: 100, width }} disabled={disabled} allowClear />
     </Form.Item>
   );
 };
