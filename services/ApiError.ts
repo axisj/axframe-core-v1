771bc9d356bacad889836edcc3de4f1318fc3a6c
tom <tom@axisj.com>
2023-08-30 14:43:48 +0900

    Fix ApiError

--- services/ApiError.ts
+++ services/ApiError.ts
@@ -1,8 +1,10 @@
 export class ApiError extends Error {
   public code: number;
+  public data?: any;
 
-  public constructor(code: number, message?: string) {
+  public constructor(code: number, message?: string, data?: any) {
     super(message);
     this.code = code;
+    this.data = data;
   }
 }
